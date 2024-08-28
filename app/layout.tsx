6e6ad708b19550d516e3fd722631eacad68a4e7a
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-27 19:37:14 -0600

    Add toaster notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <body
           className={`${inter.className} min-h-screen h-screen overflow-hidden flex flex-col`}
         >
+          <Toaster />
           {children}
         </body>
       </html>
